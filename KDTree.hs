1c31cc485d428174fb6eeed47b91d215ba4fb338
Andrew <rmansorokin@gmail.com>
2018-03-18 20:19:37 +0300

    Changed insert. Broke fromNodesList

--- KDTree.hs
+++ KDTree.hs
@@ -34,15 +34,19 @@ find = ((reverse <$>) .) . find'
       | n ! s < xn ! s = (LeftChild x r :) <$> find n l
       | otherwise = (RightChild x l :) <$> find n r
 
---эта функция должна стать вспомогательной!!
---(при вставке нового значения в дерево, мы не должны думать про splitsAt)
-insert :: (Ord k) => Int -> Int -> Node k -> KDTree k -> KDTree k
-insert d sp n Empty = Fork Empty (KDNode {node = n, splitsAt = sp `mod` d}) Empty
-insert d sp n t@(Fork l x@KDNode{node = xn, splitsAt = s} r)
-  | n == xn = t
-  | n ! s < xn ! s = Fork (insert d (sp + 1) n l) x r
-  | otherwise = Fork l x (insert d (sp + 1) n r)
+{-обдумать следующие стратегии работы с деревом:
+1)Каждый раз после вставки возвращаться к вершине дерева,
+и в следующий раз начинать поиск с вершины
+2)Каждый раз после вставки просто оставаться в зиппере,
+и в следующий раз продолжать поиск(вниз или вверх) с текущего места
+-} 
+insert :: (Ord k) => Int -> Node k -> KDTree k -> Zipper k
+insert d n t = case find n t of
+    (Empty, bs) -> let newNode = KDNode n (length bs `mod` d)
+                   in (Fork Empty newNode Empty, bs)
+    z           -> z
 
 
+{-переписать!
 fromNodesList :: (Ord k) => Int -> [Node k] -> KDTree k
-fromNodesList d = foldr (insert d 0) Empty
+fromNodesList d = foldr (insert d 0) Empty-}
