448d176483b5c68ff240e47f706ecd845e41dd5c
mrRomanovsky <rmansorokin@gmail.com>
2018-04-01 13:52:39 +0300

    added insert without zipper + reading from list

--- KDAlgo.hs
+++ KDAlgo.hs
@@ -4,15 +4,27 @@ import KDTree
 import KDZipper
 import Data.Array.IArray
 
-find :: (Ord k) => Node k -> KDTree k -> Zipper k
-find = ((reverse <$>) .) . find'
+
+insert :: (Ord k) => Int -> Int -> Node k -> KDTree k -> KDTree k
+insert d sp n Empty = Fork Empty (KDNode {node = n, splitsAt = sp `mod` d}) Empty
+insert d sp n t@(Fork l x@KDNode{node = xn, splitsAt = s} r)
+  | n == xn = t
+  | n ! s < xn ! s = Fork (insert d (sp + 1) n l) x r
+  | otherwise = Fork l x (insert d (sp + 1) n r) 
+
+
+
+findZipper :: (Ord k) => Node k -> KDTree k -> Zipper k
+findZipper = ((reverse <$>) .) . findZipper'
   where
-    find' :: (Ord k) => Node k -> KDTree k -> Zipper k
-    find' n Empty = (Empty, [])
-    find' n t@(Fork l x@KDNode{node = xn, splitsAt = s} r)
+    findZipper' :: (Ord k) => Node k -> KDTree k -> Zipper k
+    findZipper' n Empty = (Empty, [])
+    findZipper' n t@(Fork l x@KDNode{node = xn, splitsAt = s} r)
       | n == xn = (t, [])
-      | (n ! s) < (xn ! s) = (LeftChild x r :) <$> find n l
-      | otherwise = (RightChild x l :) <$> find n r
+      | (n ! s) < (xn ! s) = (LeftChild x r :) <$> findZipper n l
+      | otherwise = (RightChild x l :) <$> findZipper n r
+
+
 
 {-обдумать следующие стратегии работы с деревом:
 1)Каждый раз после вставки возвращаться к вершине дерева,
@@ -20,13 +32,12 @@ find = ((reverse <$>) .) . find'
 2)Каждый раз после вставки просто оставаться в зиппере,
 и в следующий раз продолжать поиск(вниз или вверх) с текущего места
 -} 
-insert :: (Ord k) => Int -> Node k -> KDTree k -> Zipper k
-insert d n t = case find n t of
+insertZipper :: (Ord k) => Int -> Node k -> KDTree k -> Zipper k
+insertZipper d n t = case findZipper n t of
     (Empty, bs) -> let newNode = KDNode n (length bs `mod` d)
                    in (Fork Empty newNode Empty, bs)
     z           -> z
 
 
-{-переписать!
 fromNodesList :: (Ord k) => Int -> [Node k] -> KDTree k
-fromNodesList d = foldr (insert d 0) Empty-}
+fromNodesList d = foldr (insert d 0) Empty
