753a72bafc16684a41962670dfd505422cde69b2
mrRomanovsky <rmansorokin@gmail.com>
2018-05-02 11:53:43 +0300

    presort algo: tree type fixed

--- KDPresortAlgo.hs
+++ KDPresortAlgo.hs
@@ -1,6 +1,8 @@
 --import KDTree
 --import KDAlgo
+import Data.Array.IArray
 import Data.List
+import KDTree
 
 getSorts :: Ord k => Int -> [[k]] -> [[[k]]]
 getSorts d src = sortRotate <$> [0..(d-1)]
@@ -11,17 +13,17 @@ rotate :: Int -> [a] -> [a]
 rotate _ [] = []
 rotate n xs = zipWith const (drop n (cycle xs)) xs
 
-data Tree k = Empty | Fork (Tree k) [k] (Tree k)
+lToArr tl x = (listArray (0, tl - 1) x)
 
-buildTree :: Ord k => [[[k]]] -> Int -> Int -> Tree k
+buildTree :: Ord k => [[[k]]] -> Int -> Int -> KDTree k
 buildTree l i tl = case l !! i of
   []         -> Empty
-  (x:[])     -> Fork Empty x Empty
-  (x:y:[])   -> Fork (Fork Empty x Empty) y Empty
-  (x:y:z:[]) -> Fork (Fork Empty x Empty) y (Fork Empty z Empty)
+  (x:[])     -> Fork Empty (lToArr tl x) Empty
+  (x:y:[])   -> Fork (Fork Empty (lToArr tl x) Empty) (lToArr tl x) Empty
+  (x:y:z:[]) -> Fork (Fork Empty (lToArr tl x) Empty) (lToArr tl x) (Fork Empty (lToArr tl x) Empty)
   ns         -> let m        = ns !! (length ns `div` 2)
                     (pl, ph) = foldr part_acc ([], []) l
                     part_acc xs (ls, hs) = let (lxs, (x:hxs)) = partition (<m) xs
                                                in (lxs : ls, hxs : hs)
                     next_idx = (i + 1) `mod` tl
-                    in Fork (buildTree pl next_idx tl) m (buildTree ph next_idx tl) 
+                    in Fork (buildTree pl next_idx tl) (lToArr tl m) (buildTree ph next_idx tl) 
